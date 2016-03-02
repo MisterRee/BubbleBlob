3795fcc8698ffc984aa95704b4f795966c076c47
Danny Lee <dxl1720@rit.edu>
2016-03-02 02:36:28 -0500

    basic movement added

--- src/Bubble.js
+++ src/Bubble.js
@@ -1,13 +1,11 @@
-(function (){
-	'use strict';
 
-	function Bubble(px,py,vx,vy){
-		this.position = {x:px, y:py};
-		this.velocity = {x:vx, y:vy};
-		
-		this.update = function(){
-			this.position.x += this.velocity.x;
-			this.position.y += this.velocity.y;
-		};
-	}
-}());
+function Bubble(px,py,vx,vy){
+	this.position = {x:px, y:py};
+	this.velocity = {x:vx, y:vy};
+	this.radius = 5;
+	
+	this.update = function(){
+		this.position.x += this.velocity.x;
+		this.position.y += this.velocity.y;
+	};
+}
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var socketio = require('socket.io');
-var orbObject = require('./Bubble.js');
+//var orbObject = require('./Bubble.js');
 var port = process.env.PORT || process.env.NODE_PORT || 3000;
 var index = fs.readFileSync(__dirname + '/../client/client.html');
 
@@ -15,13 +15,58 @@ var app = http.createServer(onRequest).listen(port);
 console.log("Listening on 127.0.0.1:" + port);
 var io = socketio(app);
 
-var sDraws = {};
+// Server globals
+var users = [];
+var sDraws = [];
 
 io.sockets.on('connection', function(socket){
 	socket.join('main'); // There's only one room
+	users.push(socket);
 	start(socket);
 });
 
 function start(socket){
-	socket.emit('startDraw');
+	createInitialBubbles(2);
+	setInterval(loop, 60);
 }
+
+	function createInitialBubbles(num){
+		for(var i = 0; i < num; i++){
+			var tempBubble = new Bubble(generateNumber(0, 500),
+									    generateNumber(0, 500),
+									    Math.random(),
+									    Math.random());
+			sDraws.push(tempBubble);
+		}
+	}
+	
+function loop(){
+	serverUpdate();
+	io.sockets.emit('draw', sDraws);
+}
+
+	// magic happens here
+	function serverUpdate(){
+		for(var i = 0; i < sDraws.length; i++){
+			sDraws[i].position.x += sDraws[i].velocity.x;
+			sDraws[i].position.y += sDraws[i].velocity.y;
+		}
+	}
+
+// Inclusive on min and max
+function generateNumber(min, max){
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/* Class here for the moment */
+function Bubble(px,py,vx,vy){
+	this.position = {x:px, y:py};
+	this.velocity = {x:vx, y:vy};
+	this.radius = 5;
+	
+	this.update = function(){
+		this.position.x += this.velocity.x;
+		this.position.y += this.velocity.y;
+	};
+}
+
